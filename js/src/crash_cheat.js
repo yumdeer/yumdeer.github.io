8368bd0c4f6fca12d8c5632b8cfbafc8894c8d07
jeffchal <zhangfc.com@hotmail.com>
2019-05-04 15:07:00 +0800

    Site updated: 2019-05-04 15:06:59

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,14 +1,14 @@
-<!--崩溃欺骗-->
+<!--????-->
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.ico");
          document.title = '🌙你应该是一场梦';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.ico");
          document.title = '☀我应该是一阵风' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
